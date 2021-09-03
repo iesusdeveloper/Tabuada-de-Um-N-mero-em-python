da2e2c377fdaeba1e1f42186a5c570d8e9c6c3ac
Iesus Magdiel <76500125+iesusdeveloper@users.noreply.github.com>
2021-09-03 16:26:13 -0300

    Add files via upload

---
+++ ex008.py
@@ -0,0 +1,5 @@
+#  FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO QUALQUER E MOSTRE NA TELA A SUA TABUADA
+n = int(input('Digite o número o qual você deseje saber a tabuada do mesmo: '))
+print('{nt}x1 = {n1} \n{nt}x2 = {n2} \n{nt}x3 = {n3} \n{nt}x4 = {n4} \n{nt}x5 = {n5} \n{nt}x6 = {n6} \n{nt}x7 = {n7} \n{nt}x8 = {n8} \n{nt}x9 = {n9} \n{nt}x10 = {n10}'.format(
+    nt=n, n1=n*1, n2=n*2, n3=n*3, n4=n*4, n5=n*5, n6=n*6, n7=n*7, n8=n*8, n9=n*9, n10=n*10
+))
